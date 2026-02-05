b12f4f71ed7ad5f90c31b894c5d7db9cc14d68a5
Assora42 <mehdirabehi42@gmail.com>
2026-02-05 12:40:31 +0000

    vv

--- config_TP1.py
+++ config_TP1.py
@@ -20,11 +20,17 @@ display_time_stats = True
 
 # initialization : create and place robots at initial positions (returns a list containing the robots)
 
-import robot_dumb
+import robot_braitenberg_avoider as gg
+import subsomption as gb
+import subsomption as ga
+import subsomption as gc
 
 def initialize_robots(arena_size=-1, particle_box=-1): # particle_box: size of the robot enclosed in a square
     #x_center = arena_size // 2 - particle_box / 2
     y_center = arena_size // 2 - particle_box / 2
     robots = []
-    robots.append(robot_dumb.Robot_player(4, y_center, 0, name="My Robot", team="A"))
+    robots.append(gc.Robot_player(4, y_center, 0, name="My Robot", team="A"))
+    robots.append(ga.Robot_player(7, y_center, 0, name="My Robot", team="B"))
+    robots.append(gb.Robot_player(10, y_center, 0, name="My Robot", team="C"))
+    robots.append(gg.Robot_player(13, y_center, 0, name="My Robot", team="D"))
     return robots
